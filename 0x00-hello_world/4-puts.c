3501138c5cd19260f714dc31f1f12e0045cf4fa6
Akimthedream2014 <kens2965@gmail.com>
2023-10-05 05:54:21 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 /**
- *Main Entry point"Programming is like building a multilingual
- *puzzle, followed by a new line."
- * Return: Always 0 (Success) 
-*/
-
+ * main - Entry point of the program
+ *
+ * This function prints the message "Programming is like building a multilingual puzzle"
+ * followed by a newline using the puts function and returns 0.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return (0);
+    // Define a string containing the message
+    char str[] = "Programming is like building a multilingual puzzle";
+
+    // Use the puts function to print the message with a newline
+    puts(str);
+
+    // Return 0 to indicate successful execution
+    return (0);
 }
