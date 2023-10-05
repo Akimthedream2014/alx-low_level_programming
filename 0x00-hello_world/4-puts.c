7870994fc8b02051dc6aa8bba74c8b4d83655e82
Akimthedream2014 <kens2965@gmail.com>
2023-10-05 06:09:02 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,10 +9,6 @@
  */
 int main(void)
 {
-	// Use the puts function to print the message with a newline
-
 	puts("Programming is like building a multilingual puzzle");
-
-	// Return 0 to indicate successful execution
 	return (0);
 }
